eade1c05ca552254d0a8db9995746fe194e78b59
Mattia Lenza <m.lenza@nrgsys.it>
2022-02-06 16:41:15 +0100

    small fix

--- src/main/java/it/univaq/disim/mwt/letsjamrestapi/services/AuthApiService.java
+++ src/main/java/it/univaq/disim/mwt/letsjamrestapi/services/AuthApiService.java
@@ -14,11 +14,11 @@ import javax.ws.rs.core.UriInfo;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2022-01-07T15:13:39.019Z[GMT]")
 public abstract class AuthApiService {
-    public abstract Response addUser(NewUser body, SecurityContext securityContext, UriInfo uriInfo) throws NotFoundException, SQLException;
+    public abstract Response addUser(NewUser body, SecurityContext securityContext, UriInfo uriInfo) throws NotFoundException, SQLException, ApiException;
 
     public abstract Response login(AuthLoginBody body, SecurityContext securityContext, UriInfo uriInfo) throws NotFoundException, ApiException;
 
-    public abstract Response logout(SecurityContext securityContext, ContainerRequestContext req) throws NotFoundException, SQLException;
+    public abstract Response logout(SecurityContext securityContext, ContainerRequestContext req) throws NotFoundException, SQLException, ApiException;
 
     public abstract Response refreshToken(SecurityContext securityContext, ContainerRequestContext req) throws NotFoundException;
 }
